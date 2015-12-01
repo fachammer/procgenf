8dfda007047683691828a00b071841a399da43ed
Fabian Achammer <fabian.achammer@gmail.com>
2015-12-01 23:51:14 +0100

    setup project reference to power-voronoi-diagram through gradle

---
+++ main/src/api/java/com/fabianachammer/procgenf/main/Main.java
@@ -0,0 +1,10 @@
+package com.fabianachammer.procgenf.main;
+
+import kn.uni.voronoitreemap.diagram.PowerDiagram;
+
+public class Main {
+    public static void main(String[] args) {
+	PowerDiagram diagram;
+	System.out.println("Hello World with PowerDiagram");
+    }
+}
