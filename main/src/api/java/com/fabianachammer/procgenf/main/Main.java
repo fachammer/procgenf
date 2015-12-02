c77786ccb1ed5b8aa9dbcd3e9b17cec5aba93075
Fabian Achammer <fabian.achammer@gmail.com>
2015-12-02 11:40:27 +0100

    removed LWJGL dependency and updated power voronoi diagram

--- main/src/api/java/com/fabianachammer/procgenf/main/Main.java
+++ main/src/api/java/com/fabianachammer/procgenf/main/Main.java
@@ -1,10 +1,13 @@
 package com.fabianachammer.procgenf.main;
 
+import kn.uni.voronoitreemap.datastructure.OpenList;
 import kn.uni.voronoitreemap.diagram.PowerDiagram;
 
 public class Main {
     public static void main(String[] args) {
-	PowerDiagram diagram;
-	System.out.println("Hello World with PowerDiagram");
+    	PowerDiagram diagram = new PowerDiagram();
+    	OpenList sites = new OpenList();
+    	diagram.setSites(sites);
+    	System.out.println("Hello World with PowerDiagram");
     }
 }
