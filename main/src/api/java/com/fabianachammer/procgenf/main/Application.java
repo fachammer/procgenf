9c04e8f0b6b926243fdeb51701a3bddd5d53a384
Fabian Achammer <fabian.achammer@gmail.com>
2015-12-07 14:12:41 +0100

    removed JavaFX code, added jdk8 to travis, added LWJGL dependencies

---
+++ main/src/api/java/com/fabianachammer/procgenf/main/Application.java
@@ -0,0 +1,5 @@
+package com.fabianachammer.procgenf.main;
+
+public interface Application {
+	void run(String[] args);
+}
---
+++ main/src/impl/java/com/fabianachammer/procgenf/main/impl/ApplicationImpl.java
@@ -0,0 +1,10 @@
+package com.fabianachammer.procgenf.main.impl;
+
+import com.fabianachammer.procgenf.main.Application;
+
+public class ApplicationImpl implements Application {
+	
+	public void run(String[] args) {
+		
+	}
+}
